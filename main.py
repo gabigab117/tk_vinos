c49a243d1f9e113fbda0412cafe6e6128e196a0e
gabigab117 <gabrieltrouve5@gmail.com>
2024-06-11 21:45:43 +0200

    replace tkinter objects by customtkinter objects

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from tkinter import Tk, Button, Label, Frame
+from customtkinter import CTk, CTkButton, CTkLabel, CTkFrame
 from vinos import DO_VINOS
 from tkintermapview import TkinterMapView
 from PIL.ImageTk import PhotoImage
 from PIL import Image
 
 
-class Vinos(Tk):
+class Vinos(CTk):
     def __init__(self):
         super().__init__()
-        label = Label(self, text="Vinos Ibericos")
+        label = CTkLabel(self, text="Vinos Ibericos")
         label.pack()
-        self.left_container = Frame(self, width=100, height=1200, relief="raised", borderwidth=1)
+        self.left_container = CTkFrame(self, width=100, height=1200)
         self.left_container.pack(side="left")
         self._display_vinos_buttons()
         self.geometry("1200x1200")
@@ -19,7 +19,7 @@ class Vinos(Tk):
 
     def _display_vinos_buttons(self):
         for k, v in DO_VINOS.items():
-            button = Button(self.left_container, text=k,
+            button = CTkButton(self.left_container, text=k,
                             command=lambda x=v[0][0], y=v[0][1], color=v[1]: self._display_marker(x, y, color))
             button.pack()
 
