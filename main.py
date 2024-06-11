d51f6b38ec9632c9ac52f8f8ad6ddb236540a678
gabigab117 <gabrieltrouve5@gmail.com>
2024-06-11 22:44:47 +0200

    add colors to the app :)

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ class Vinos(CTk):
         super().__init__()
         label = CTkLabel(self, text="Vinos Ibericos")
         label.pack()
-        self.left_container = CTkFrame(self, width=100, height=1200)
-        self.left_container.pack(side="left")
+        self.left_container = CTkFrame(self, width=100, height=1200, fg_color="white")
+        self.left_container.pack(side="left", padx=50)
         self._display_vinos_buttons()
         self.geometry("1200x1200")
         self.title("Vinos Ibericos")
         self.map = self._display_map_widget()
 
     def _display_vinos_buttons(self):
+        red = "#f5a6a8"
+        white = "#f0d795"
         for k, v in DO_VINOS.items():
+            fg_button_color = white if v[1] == "Blanco" else red
             button = CTkButton(self.left_container, text=k,
-                            command=lambda x=v[0][0], y=v[0][1], color=v[1]: self._display_marker(x, y, color))
-            button.pack()
+                               command=lambda x=v[0][0], y=v[0][1], color=v[1]: self._display_marker(x, y, color),
+                               fg_color=fg_button_color, text_color="black", hover_color="yellow")
+            button.pack(padx=5, pady=10)
 
     def _display_map_widget(self):
         map_widget = TkinterMapView(self, width=1100, height=1200, corner_radius=1)
@@ -38,4 +42,5 @@ class Vinos(CTk):
 
 
 window = Vinos()
+window.configure(fg_color="#e6deca")
 window.mainloop()
